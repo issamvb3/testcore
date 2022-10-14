f7e173a759a3730762e36e90af4253fdcc066cd1
CoderGears <support@codergears.com>
2022-10-14 21:51:04 +0100

    Update Class1.cs

--- ClassLibrary2/Class1.cs
+++ ClassLibrary2/Class1.cs
@@ -7,8 +7,7 @@ namespace ClassLibrary2
      
         public void test()
         {
-                                
-                         
+                                                     
             int a=0;                         
 
             a++;
