c407bf1e756f3b1171a871ee067568ab7995354e
Issam Lahlali <32836518+issamvb3@users.noreply.github.com>
2024-06-04 11:37:10 +0100

    Update Class1.cs

--- ClassLibrary2/Class1.cs
+++ ClassLibrary2/Class1.cs
@@ -5,8 +5,7 @@ namespace ClassLibrary2
     public class Class1
     {       
         public void test()
-        {      
-            
+        {          
             int a=0;        
             a++;
              a++;
